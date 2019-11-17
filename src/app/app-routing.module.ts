fe77b0828cd8248c92759a69d87eb0adcc7b84c1
Sabrina Bourouis <bourouis.sabrina@laposte.net>
2019-11-17 08:48:20 +0100

    feat: added route /project/:id

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import {ProjectsComponent} from './views/projects/projects.component';
 import {AboutComponent} from './views/about/about.component';
 import {ContactComponent} from './views/contact/contact.component';
+import { ProjectComponent } from './views/project/project.component';
 
 const routes: Routes = [
   {
     path: 'projects',
     component: ProjectsComponent
+  },
+  {
+    path: 'projects/:projectId',
+    component: ProjectComponent
   },
   {
     path: 'about',
